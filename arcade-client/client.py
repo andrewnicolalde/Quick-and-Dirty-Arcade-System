650c0bf01b65b185554e83634df6fbbc9e7ab965
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2019-02-03 04:08:13 +0000

    Add dynamic game-switching

--- arcade-client/client.py
+++ arcade-client/client.py
@@ -6,37 +6,46 @@ import time
 import os
 import signal
 
+# Dict of acceptable titles to play.
+title_command = {
+    "Robot Bowl": "robotbwl",
+    "Car Polo": "carpolo"
+}
+
+print(title_command)
+
 # Make initial request
 with urllib.request.urlopen("https://storage.googleapis.com/arcademachine/title.html") as response:
-        title = response.read()
+    title = response.read().decode('utf-8')
+    print(title)
     
     # Store first response to avoid repeatedly launching same process
-current_title = title
+    current_title = title
+    print(current_title)
 
-# No current title checks for first run
-if title == b"robotbwl":
-    # game_proc = subprocess.Popen(["mame", "robotbwl"], shell=False, preexec_fn=os.setsid)
-    game_proc = subprocess.Popen(["mame", "robotbwl"], shell=False)
-    print(game_proc.pid)
-else:
-    print("The value of the page is NOT robotbwl")
-    
-time.sleep(4)
+    # No current title checks for first run
+    print(title in title_command)
+    if title in title_command:
+        game_proc = subprocess.Popen(["mame", title_command[title]], shell=False)
+        print(game_proc.pid)
+    else:
+        print("Invalid title")
+
+    time.sleep(4)
 
 # Repeatedly check status of current title
 while True:
     with urllib.request.urlopen("https://storage.googleapis.com/arcademachine/title.html") as response:
-        title = response.read()
+        title = response.read().decode('utf-8')
 
     # If the latest title is different from the current title, launch the game
-    if title == b"robotbwl":
+    if title in title_command:
         if current_title != title:
             subprocess.Popen(["kill", "-9", str(game_proc.pid)], shell=False) # Murder MAME because it doesn't seem to accept regular kills
-            game_proc = subprocess.Popen(["mame", "robotbwl"], shell=False) 
+            game_proc = subprocess.Popen(["mame", title_command[title]], shell=False)
+            current_title = title
         else:
             print("The current game is already", title)
-    else:
-        print("The value of the page is NOT robotbwl, killing current game process.")
-        subprocess.Popen(["kill", "-9", str(game_proc.pid)], shell=False) # Murder MAME because it doesn't seem to accept regular kills
+    
     
     time.sleep(4)
